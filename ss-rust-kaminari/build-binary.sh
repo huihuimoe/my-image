c4d9d474fa867f22a272e4739d367237eee5fa1b
huihuimoe <github@huihui.contact>
2024-08-11 13:28:05 +0900

    use kaminari master branch instead

--- ss-rust-kaminari/build-binary.sh
+++ ss-rust-kaminari/build-binary.sh
@@ -36,7 +36,7 @@ for short_target in ${TARGET[@]}; do
   target="${short_target}-unknown-linux-gnu"
   cross build --target $target \
               --no-default-features \
-              --features 'basic, mimalloc, local-tunnel, aead-cipher-2022, local-online-config' \
+              --features 'basic, mimalloc, local-tunnel, aead-cipher-2022, local-dns, local-redir, local-online-config' \
               --release
   if [[ $short_target == "x86_64" ]]; then
     arch=amd64
@@ -52,16 +52,18 @@ popd
 if [[ -d kaminari-src ]]; then
   rm -rf kaminari-src
 fi
-git clone https://github.com/zephyrchien/kaminari -b v$KAMINARI_VERSION --depth=1 kaminari-src
+# git clone https://github.com/zephyrchien/kaminari -b v$KAMINARI_VERSION --depth=1 kaminari-src
+git clone https://github.com/zephyrchien/kaminari --depth=1 kaminari-src
 pushd kaminari-src
 cargo update --breaking -Z unstable-options \
   kaminari realm_io webpki-roots
 cargo update
 # fix impl_trait_in_assoc_type
-sed -i '1s|^|#![feature(impl_trait_in_assoc_type)]\n|' kaminari/src/lib.rs
+# sed -i '1s|^|#![feature(impl_trait_in_assoc_type)]\n|' kaminari/src/lib.rs
 # fix breaking realm_io
-sed -i 's|-> std::io::Result<()>|-> std::io::Result<(u64, u64)>|' \
-  cmd/src/client.rs cmd/src/server.rs
+# sed -i 's|-> std::io::Result<()>|-> std::io::Result<(u64, u64)>|' \
+#   cmd/src/client.rs cmd/src/server.rs
+
 for short_target in ${TARGET[@]}; do
   target="${short_target}-unknown-linux-gnu"
   cross build --target $target \
