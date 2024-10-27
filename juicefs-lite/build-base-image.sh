f199ef28f35dc272b957dc71b04385da192a6230
huihuimoe <0w0@loli.pet>
2024-10-27 16:20:14 +0900

    fix localhost registry with buildx

--- juicefs-lite/build-base-image.sh
+++ juicefs-lite/build-base-image.sh
@@ -9,8 +9,12 @@ docker run \
   cgr.dev/chainguard/apko \
     build \
     juicefs-base.yaml \
-    localhost/juicefs-base:latest \
+    localhost:5000/juicefs-base:latest \
     juicefs-base.tar
 
+# https://github.com/docker/buildx/issues/301#issuecomment-755164475
+docker run -d --name registry --network=host registry:2
 docker load < juicefs-base.tar
+docker push localhost:5000/juicefs-base:latest-amd64
+docker push localhost:5000/juicefs-base:latest-arm64
 docker images
